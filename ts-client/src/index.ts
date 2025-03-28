19d9c05700709471da03955f5b916b88f9a61fbb
docongminh <cristiando0902@gmail.com>
2025-03-28 09:47:36 +0700

    export idl

--- ts-client/src/index.ts
+++ ts-client/src/index.ts
@@ -4,3 +4,6 @@ export * from "./types";
 export * from "./constants";
 export * from "./utils";
 export * from "./types";
+export type { CpAmm as CpAmmTypes } from "./idl/cp_amm";
+import CpAmmIDL from "./idl/cp_amm.json";
+export default CpAmmIDL;
